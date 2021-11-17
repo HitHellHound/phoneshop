6f0123452a37634617d4eaf703d933d3670be20f
Ignat Churilo <i.ignatch.com@mail.ru>
2021-11-18 00:58:08 +0300

    Implement JDBCProductDAO and Tests

--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -1,6 +1,5 @@
 package com.es.core.model.phone;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -9,19 +8,50 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class JdbcPhoneDao implements PhoneDao{
+public class JdbcPhoneDao implements PhoneDao {
     @Resource
     private JdbcTemplate jdbcTemplate;
+    @Resource
+    private PhoneRowMapper phoneRowMapper;
+
+    private static final String GET_QUERY = "SELECT ph.*, colors.code AS color " +
+            "FROM (SELECT phones.* FROM phones WHERE phones.id = ?) AS ph " +
+            "LEFT JOIN phone2color ON ph.id = phone2color.phoneId " +
+            "LEFT JOIN colors ON phone2color.colorId = colors.id";
+    private static final String SAVE_INSERT_QUERY = "INSERT INTO phones (Id, Brand, Model, Price, DisplaySizeInches, " +
+            "WeightGr, LengthMm, WidthMm, HeightMm, Announced, DeviceType, Os, DisplayResolution, PixelDensity, " +
+            "DisplayTechnology, BackCameraMegapixels, FrontCameraMegapixels, RamGb, InternalStorageGb, BatteryCapacityMah, " +
+            "TalkTimeHours, StandByTimeHours, Bluetooth, Positioning, ImageUrl, Description) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String FIND_ALL_QUERY = "select ph.*, colors.code as color " +
+            "from (select phones.* from phones offset ? limit ?) ph " +
+            "left join phone2color on ph.id = phone2color.phoneId " +
+            "left join colors on phone2color.colorId = colors.id";
+    private static final String SELECT_COLOR_BY_CODE_QUERY = "SELECT colors.id FROM colors WHERE colors.code = ?";
+    private static final String INSERT_NEW_COLOR_DEPENDENCE_QUERY = "INSERT INTO phone2color (phoneId, colorId) VALUES (?, ?)";
 
     public Optional<Phone> get(final Long key) {
-        throw new UnsupportedOperationException("TODO");
+        return jdbcTemplate.query(GET_QUERY, phoneRowMapper, key).stream().findAny();
     }
 
     public void save(final Phone phone) {
-        throw new UnsupportedOperationException("TODO");
+        jdbcTemplate.update(SAVE_INSERT_QUERY,
+                phone.getId(), phone.getBrand(), phone.getModel(), phone.getPrice(), phone.getDisplaySizeInches(),
+                phone.getWeightGr(), phone.getLengthMm(), phone.getWidthMm(), phone.getHeightMm(),
+                phone.getAnnounced(), phone.getDeviceType(), phone.getOs(), phone.getDisplayResolution(),
+                phone.getPixelDensity(), phone.getDisplayTechnology(), phone.getBackCameraMegapixels(),
+                phone.getFrontCameraMegapixels(), phone.getRamGb(), phone.getInternalStorageGb(),
+                phone.getBatteryCapacityMah(), phone.getTalkTimeHours(), phone.getStandByTimeHours(),
+                phone.getBluetooth(), phone.getPositioning(), phone.getImageUrl(), phone.getDescription());
+
+        phone.getColors().stream()
+                .forEach(color -> {
+                    Long colorId = jdbcTemplate.queryForObject(SELECT_COLOR_BY_CODE_QUERY, Long.class, color.getCode());
+                    jdbcTemplate.update(INSERT_NEW_COLOR_DEPENDENCE_QUERY, phone.getId(), colorId);
+                });
     }
 
     public List<Phone> findAll(int offset, int limit) {
-        return jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit, new BeanPropertyRowMapper(Phone.class));
+        return jdbcTemplate.query(FIND_ALL_QUERY, phoneRowMapper, offset, limit);
     }
 }
---
+++ core/src/test/java/com/es/core/model/phone/JdbcPhoneDaoTest.java
@@ -0,0 +1,67 @@
+package com.es.core.model.phone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/context/testApplicationContext.xml")
+public class JdbcPhoneDaoTest {
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+    @Resource
+    private PhoneDao phoneDao;
+
+    @Test
+    public void testSavePhoneInDB() {
+        Phone testPhone = new Phone();
+        testPhone.setId(1L);
+        testPhone.setBrand("testBrand1");
+        testPhone.setModel("testModel1");
+        Color color = new Color();
+        color.setCode("Black");
+        HashSet<Color> testColors = new HashSet<>();
+        testColors.add(color);
+        testPhone.setColors(testColors);
+        phoneDao.save(testPhone);
+
+        Phone phone = jdbcTemplate.query("select ph.*, colors.code as color " +
+                        "from (select phones.* from phones where phones.id = 1) ph " +
+                        "left join phone2color on ph.id = phone2color.phoneId " +
+                        "left join colors on phone2color.colorId = colors.id",
+                new PhoneRowMapper()).stream().findAny().orElse(null);
+        assertNotNull(phone);
+
+        List<Color> colors = phone.getColors().stream().collect(Collectors.toList());
+        assertTrue(colors.get(0).getCode().equals("Black"));
+    }
+
+    @Test
+    public void testGet() {
+        Phone testPhone = new Phone();
+        testPhone.setId(2L);
+        testPhone.setBrand("testBrand2");
+        testPhone.setModel("testModel2");
+        Color color = new Color();
+        color.setCode("White");
+        HashSet<Color> testColors = new HashSet<>();
+        testColors.add(color);
+        testPhone.setColors(testColors);
+        phoneDao.save(testPhone);
+
+        Phone phone = phoneDao.get(2L).orElse(null);
+        assertNotNull(phone);
+        List<Color> colors = phone.getColors().stream().collect(Collectors.toList());
+        assertTrue(colors.get(0).getCode().equals("White"));
+    }
+}
